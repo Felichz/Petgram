064305ad1ea89c7e806c6f7f06344aff6e517e3f
Felichz <andersson.fx@gmail.com>
2020-09-08 01:26:54 -0300

    Service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,78 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-module.exports = {
-    output: {
-        filename: 'app.bundle.js',
-        publicPath: '/',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-        }),
-        new WebpackPwaManifestPlugin({
-            name: 'Petgram | For animal lovers!',
-            shortname: 'Petgram 🐱',
-            description:
-                'Petgram is a place to explore amazing photos and share that what makes your pet special. And yes, there are a lot of kitten photos here too!',
-            background_color: '#fff',
-            theme_color: '#b1a',
-            icons: [
+module.exports = (env, { mode }) => {
+    const isDev = mode === 'development';
+
+    return {
+        output: {
+            filename: 'app.bundle.js',
+            publicPath: '/',
+        },
+        optimization: !isDev && {
+            minimizer: [new TerserPlugin()],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'src/index.html',
+            }),
+            new WebpackPwaManifestPlugin({
+                name: 'Petgram | For animal lovers!',
+                shortname: 'Petgram 🐱',
+                description:
+                    'Petgram is a place to explore amazing photos and share that what makes your pet special. And yes, there are a lot of kitten photos here too!',
+                background_color: '#fff',
+                theme_color: '#b1a',
+                icons: [
+                    {
+                        src: path.resolve('src/assets/icon.png'),
+                        sizes: [96, 128, 192, 256, 384, 512],
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: path.resolve('src/assets/apple-touch-icon.png'),
+                        sizes: [120, 152, 167, 180, 192],
+                        ios: true,
+                    },
+                ],
+            }),
+            new WorkboxWebpackPlugin.GenerateSW({
+                runtimeCaching: [
+                    {
+                        urlPattern: new RegExp(
+                            'https://(res.cloudinary.com|images.unsplash.com)'
+                        ),
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'images',
+                        },
+                    },
+                    {
+                        urlPattern: new RegExp(
+                            'https://petgram-server-hmcpzh3ov.vercel.app/'
+                        ),
+                        handler: 'NetworkFirst',
+                        options: {
+                            cacheName: 'api',
+                        },
+                    },
+                ],
+            }),
+        ],
+        module: {
+            rules: [
                 {
-                    src: path.resolve('src', 'assets', 'icon.png'),
-                    sizes: [96, 128, 192, 256, 384, 512],
-                    purpose: 'any maskable',
-                },
-                {
-                    src: path.resolve('src', 'assets', 'apple-touch-icon.png'),
-                    sizes: [96, 128, 192],
-                    ios: true,
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
                 },
             ],
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-        ],
-    },
-    devServer: {
-        historyApiFallback: true,
-    },
+        },
+        devServer: {
+            historyApiFallback: true,
+        },
+    };
 };
